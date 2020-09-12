ec3078a972526d9a72c547e55c4dbc9ded870f7d
serjukl <serjukl98@gmail.com>
2020-09-12 13:45:01 +0300

    minify images

--- src/Components/MenuItems/MenuItems.jsx
+++ src/Components/MenuItems/MenuItems.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import ItemPopup from "../ItemPopup/ItemPopup";
+import {Helmet} from "react-helmet";
 
 const MenuItems = props => {
 
     return (
-        <div className={'MenuItems'}>
-            <div className="itemsContainer">
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Lecler | каталог товарів</title>
+                <link rel="canonical" href="http://lecler.com.ua" />
+            </Helmet>
+            <div className={'MenuItems'}>
+                <div className="itemsContainer">
+                    {
+                        props.data ?
+                            props.data.items.map((item, index) => {
+                                return (
+                                    <div
+                                        id={index}
+                                        onClick={() => props.setPopup({status: true, init: item})}
+                                        key={index}
+                                        className="item"
+                                        title={item.name}
+                                    >
+                                        <img src={require(`../../IMG/${item.image}`)} alt={item.name}/>
+                                        <h2>{item.name}</h2>
+                                    </div>
+                                )
+                            })
+                            : null
+                    }
+                </div>
                 {
-                    props.data ?
-                    props.data.items.map((item, index) => {
-                        return (
-                            <div
-                                id={index}
-                                onClick={() => props.setPopup({status: true, init: item})}
-                                key={index}
-                                className="item"
-                                title={item.name}
-                            >
-                                <img src={require(`../../IMG/${item.image}`)} alt={item.name}/>
-                                <h2>{item.name}</h2>
-                            </div>
-                        )
-                    })
+                    props.popup.status
+                        ? <ItemPopup
+                            dataItem={props.popup.init}
+                            allData={props.data.items}
+                            setPopup={(value) => props.setPopup(value)}
+                        />
                         : null
                 }
             </div>
-            {
-                props.popup.status
-                    ? <ItemPopup
-                    dataItem={props.popup.init}
-                    allData={props.data.items}
-                    setPopup={(value) => props.setPopup(value)}
-                    />
-                    : null
-            }
-        </div>
+        </>
     );
 };
 
